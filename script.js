99999a64e325c77b3d74d117e61553947aa83c58
Talin <talin0018@gmail.com>
2026-03-14 16:38:54 -0500

    editted buttons

--- script.js
+++ script.js
@@ -4,21 +4,27 @@ const projects = [
 title:"Evony Keep Tracker",
 description:"Tracks building progress and upgrade goals for Evony.",
 github:"#",
-demo:"#"
+demo:"#",
+button1:"GitHub",
+button2:"Live Demo"
 },
 
 {
 title:"This Portfolio Website",
 description:"Responsive developer portfolio using HTML, CSS and JS.",
-github:"https://github.com/TalinHubb/MyPortfolio",
-demo:"https://talinhubb.github.io/MyPortfolio/"
+github:"https://github.com/TalinHubb/my-portfolio",
+demo:"https://talinhubb.github.io/my-portfolio/",
+button1:"GitHub",
+button2:"This Site"
 },
 
 {
 title:"LINE Translator Bot",
 description:"Completely free to use LINE bot that translates messages with one click, using Google's translation API, and deployed on Render.",
 github:"https://lin.ee/QFj2gHg",
-demo:"https://line-translator-1-i242.onrender.com"
+demo:"https://line-translator-1-i242.onrender.com",
+button1:"Add the Bot",
+button2:"Live Demo"
 }
 
 ]
@@ -35,8 +41,8 @@ card.innerHTML = `
 <p>${project.description}</p>
 
 <div class="project-links">
-<a href="${project.github}">GitHub</a>
-<a href="${project.demo}">Try Me</a>
+<a href="${project.github}">${button1}</a>
+<a href="${project.demo}">${button2}</a>
 </div>
 `
 
