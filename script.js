5255f68a64e5157e210f6fa29192432e99d5de1d
Talin <talin0018@gmail.com>
2026-03-14 16:51:30 -0500

    wanted custom buttons for some projects

--- script.js
+++ script.js
@@ -36,14 +36,20 @@ projects.forEach(project => {
 const card = document.createElement("div")
 card.classList.add("project-card")
 
+let buttons = ""
+
+if(project.github){
+buttons += `<a href="${project.github}">${project.button1 || "GitHub"}</a>`
+}
+
+if(project.demo){
+buttons += `<a href="${project.demo}">${project.button2 || "Demo"}</a>`
+}
+
 card.innerHTML = `
 <h3>${project.title}</h3>
 <p>${project.description}</p>
-
-<div class="project-links">
-<a href="${project.github}">${project.button1}</a>
-<a href="${project.demo}">${project.button2}</a>
-</div>
+<div class="project-links">${buttons}</div>
 `
 
 container.appendChild(card)
